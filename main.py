b21706440ff13a3ab31be0033feb1e9221a2a6ad
Solohill <solomonhill0620@gmail.com>
2022-06-15 13:16:19 -0400

    Space Shooter Update

--- main.py
+++ main.py
@@ -76,6 +76,11 @@ def main():
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
     player_vel = 5
     player = Player(300, 650)
 
@@ -90,13 +95,22 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
+
+        if len(enemies) == 0:
+            level = 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -111,7 +125,10 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #down
             player.y += player_vel
+
+        for enemy in enemies:
+            enemy.move(enemy_vel)
         
-            
+        redraw_window()    
 
 main()        
